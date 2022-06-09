b9bb37ee6dc283964ce92ee66b57878f4e248a94
FerdinandLucas <ferdinand.lucas@alumni.ie.edu>
2022-06-09 20:18:09 +0200

    rarity score

--- preseNFT.js
+++ preseNFT.js
@@ -4,28 +4,28 @@
 //
 
     const Moralis = require('moralis/node')
-    const serverUrl = "https://aizve2ka1w9x.usemoralis.com:2053/server";     //Moralis Server Url here
-    const appId = "xfcgOfvlPAkEiKpDWRbGuVdVejxI3UyNvitEhKLP";                //Moralis Server App ID here
+    const serverUrl = "https://aizve2ka1w9x.usemoralis.com:2053/server";        //Moralis Server Url here
+    const appId = "xfcgOfvlPAkEiKpDWRbGuVdVejxI3UyNvitEhKLP";                   //Moralis Server App ID here
 
 Moralis.start({serverUrl, appId})
 
-    const collectionAddress = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"; //Collection Address Here
-    const collectionName = "CryptoPunks";                                   //CollectioonName Here
+    const collectionAddress = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";     //Collection Address Here
+    const collectionName = "CryptoPunks";                                       //CollectioonName Here
 
 async function createRarity() {
     const tokens = await Moralis.Web3API.token.getAllTokenIds({address: collectionAddress})
-        //console.log(tokens);                                             //1 page 
-    const collectionSize = tokens.total                                    //All NFTs in the collection 
-    const pageSize = tokens.page_size                                      //All Api-data pages / 1 page = ca 100 iterations
+        //console.log(tokens);                                                  //1 page 
+    const collectionSize = tokens.total                                         //All NFTs in the collection 
+    const pageSize = tokens.page_size                                           //All Api-data pages / 1 page = ca 100 iterations
         console.log(collectionSize);
         console.log(pageSize); 
 
-    let alltokens = tokens.result                                         //All data
+    let alltokens = tokens.result                                               //All data
 
     /* const timer = (ms) => new Promise((res) => setTimeout(res, ms));
     for (let i = pageSize; i < collectionSize; i = i + pageSize) {
         const tokens = await Moralis.Web3API.token.getAllTokenIds({   
-          address: collectionAddress, });                                  //Loop through all the Nfts in the Api
+          address: collectionAddress, });                                       //Loop through all the Nfts in the Api
         while (tokens.next){
             result = await tokens.next() //https://docs.moralis.io/moralis-dapp/web3-api#web3-api-pagination-examples-with-.next
             //console.log(tokens)
@@ -71,24 +71,34 @@ for (let i = 0; i < tokenTraits.length; i++) {
       } else {
         TC[now][nowValue] = 1;
       }
-    }
-   
-                                                                                
+    }                                                                               
 }
-
                                                                                 //NOW we have to start calculating the given data and get rarity scores to sort ;)
 for (let j = 0; j < meta.length; j++) {
     let now = meta[j];
     let totalRarity = 0;
     for (let i = 0; i < now.length; i++) {
       let rarityScore =
-        1 / (TC[now[i].trait_type][now[i].value] / collectionSize); // number of traits, devide by collection = rarity% |  1 / rarity%  = rarity score
+        1 / (TC[now[i].trait_type][now[i].value] / collectionSize);             // number of traits, devide by collection = rarity% |  1 / rarity%  = rarity score
       now[i].rarityScore = rarityScore;
       totalRarity += rarityScore;
     }
-    console.log(meta[0])
-}
+  
+
+
+                                                                            //now rarity score for how many traits a token got 
+
+let numTraitsRarityScore =
+8 * (1 / (TC.TraitCount[Object.keys(now).length] / collectionSize));
+now.push({
+trait_type: "TraitCount",
+value: Object.keys(now).length,
+rarityScore: numTraitsRarityScore,
+});
+totalRarity += numTraitsRarityScore;   
+
+console.log(meta[0])
 }
 
-    
+}
 createRarity();
